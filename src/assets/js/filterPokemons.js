2e373b93aac8b973ee4c134a0fe293d95ff8e27c
Moses Bolofo (ZA) <moses.bolofo@absa.africa>
2021-08-09 09:59:12 +0200

    updated filters search

--- src/assets/js/filterPokemons.js
+++ src/assets/js/filterPokemons.js
@@ -4,13 +4,14 @@ function filterPokemons() {
     input = document.getElementById("searchText");
     filter = input.value.toUpperCase();
     pokeRow = document.getElementById("pokeRow");
-    matCards = pokeRow.getElementsByTagName("a");
+    matCards = pokeRow.getElementsByTagName("p");
     for (i = 0; i < matCards.length; i++) {
         pokemonName = matCards[i].getElementsByTagName("h3")[0];
         if (pokemonName) {
             txtValue = pokemonName.textContent || pokemonName.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                matCards[i].style.display = "inline";
+                matCards[i].style.display = "";
+
             } else {
                 matCards[i].style.display = "none";
             }
