8927d14f5a011911de34d76f52dbf7347c1f0372
Moses Bolofo (ZA) <moses.bolofo@absa.africa>
2021-08-09 09:27:55 +0200

    Using bootstrap and material styles

--- src/assets/js/filterPokemons.js
+++ src/assets/js/filterPokemons.js
@@ -4,17 +4,13 @@ function filterPokemons() {
     input = document.getElementById("searchText");
     filter = input.value.toUpperCase();
     pokeRow = document.getElementById("pokeRow");
-    matCards = pokeRow.getElementsByTagName("p");
-    console.log(matCards.length)
+    matCards = pokeRow.getElementsByTagName("a");
     for (i = 0; i < matCards.length; i++) {
-        pokemonName = matCards[i].getElementsByTagName("mat-card-title")[0];
-        console.log("Inner: " + pokemonName.innerText);
-
-        // console.log(matCards.length);
+        pokemonName = matCards[i].getElementsByTagName("h3")[0];
         if (pokemonName) {
             txtValue = pokemonName.textContent || pokemonName.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                matCards[i].style.display = "";
+                matCards[i].style.display = "inline";
             } else {
                 matCards[i].style.display = "none";
             }
